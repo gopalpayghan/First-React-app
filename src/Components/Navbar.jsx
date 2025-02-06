6652451980a3a2dd90d0bd6acaa156ec55f8e11d
Gopal Payghan <gopaldadapayghan@gmail.com>
2025-02-06 23:42:25 +0530

    react app

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="container">
+        <div className="logo">
+          <img src="/Images/brand_logo.png" alt="logo" />
+        </div>
+        <ul>
+          <li href="#">Home</li>
+          <li href="#">Location</li>
+          <li href="#">About us</li>
+          <li href="#">Contact</li>
+        </ul>
+        <button>Login</button>
+      </nav>
+        </div>
+    );
+};
+
+export default Navbar;
