390fe0e10b70f13ca84bfb1825a2152ba54f1a2a
Gopal Payghan <gopaldadapayghan@gmail.com>
2025-02-07 22:45:33 +0530

    image of the file

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,20 +2,23 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <div>
-            <nav className="container">
-        <div className="logo">
-          <img src="/Images/brand_logo.png" alt="logo" />
-        </div>
-        <ul>
-          <li href="#">Home</li>
-          <li href="#">Location</li>
-          <li href="#">About us</li>
-          <li href="#">Contact</li>
-        </ul>
-        <button>Login</button>
-      </nav>
-        </div>
+      <div>
+        <nav className="container">
+          <div className="logo">
+            <img
+              src=".\public\Images\brand_logo.png"
+              alt="logo"
+            />
+          </div>
+          <ul>
+            <li href="#">Home</li>
+            <li href="#">Location</li>
+            <li href="#">About us</li>
+            <li href="#">Contact</li>
+          </ul>
+          <button>Login</button>
+        </nav>
+      </div>
     );
 };
 
